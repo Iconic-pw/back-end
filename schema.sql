df74832faf4730b895ada449aa4acfda93ebf402
RawanYaghmour <Rawan.yag02@gmail.com>
2024-05-17 20:18:50 +0000

    add final1

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE card (
+    id SERIAL PRIMARY KEY,
+    card_name VARCHAR(255),
+    card_category VARCHAR(255),
+    card_level VARCHAR(255),
+    job_title VARCHAR(255),
+    img VARCHAR(255) ,
+    portfolio VARCHAR(255),
+    is_fav BOOLEAN DEFAULT FALSE 
+);
+
+
+CREATE TABLE about_us (
+    id SERIAL PRIMARY KEY,
+    my_name VARCHAR(255),
+    img VARCHAR(255) ,
+    describtion VARCHAR(255),
+    portfolio VARCHAR(255)
+);
