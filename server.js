3b9de4f6c8d751c948af5b6a044af5cafb1d078f
RawanYaghmour <Rawan.yag02@gmail.com>
2024-05-17 23:08:18 +0000

    push 1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const axios = require("axios").default;
 require("dotenv").config();
 
-const movieData = require("./Movie Data/data.json");
+const cardData = require("./Card_Data/data.json");
 
 //const apiKey = process.env.API_KEY;
 const pgUrl = process.env.DATABASE_URL;
@@ -27,43 +27,68 @@ app.use(express.json());
 app.get('/', homeHandler); //For Us , Locally
 
 app.post('/addLocallyData', addLocallyDataHandler); //For Us , Locally
-app.post('/addNewCard', addNewCardHandler);
+app.post('/addNewCard', addNewCardHandler);//*
 
-app.get('/getAllCards', getCardsHandler); //For Us , Locally
+app.get('/getAllCards', getCardsHandler); //For Us , Locally //*
 app.get('/getCards/:category', getCardsByCategoryHandler);
 
-app.put('/updateCard/:id', updateCardHandler); //For Us , Locally
+app.put('/updateCard/:id', updateCardHandler); //For Us , Locally//*
 app.put('/updateFavorite/:id', updateHandler);
 
-app.delete('/deleteCard/:id', deleteCardHandler); //For Us , Locallyrrr
+app.delete('/deleteCard/:id', deleteCardHandler); //For Us , Locallyrrr //*
 
 app.use(handleServerError);
 app.use(handleNotFoundError);
 
-
+class Card {
+    constructor(card_name, card_category, card_level, job_title, img, portfolio) {
+        this.card_name = card_name;
+        this.card_category = card_category;
+        this.card_level = card_level;
+        this.job_title = job_title;
+        this.img = img;
+        this.portfolio = portfolio;
+    }
+}
 
 function homeHandler(req, res) {
     res.send("WELCOME IN OUR ICONIC PROJECT");
 }
 
 function addLocallyDataHandler(req, res) {
-    console.log(req.body)
-    //to collect the data:
-    //const title= req.body.title;
-    //const time= req.body.time;
-    //const image= req.body.image;
+    const allCardsData = cardData.map((item) => {
+        return new Card(
+            item.name,
+            item.category,
+            item.level,
+            item.job_title,
+            item.image,
+            item.portfolio
+        );
+    });
 
-    const { title, release_date, poster_path, overview, personal_comment } = req.body //destructuring ES6
-    const sql = `INSERT INTO tMovie (title, release_date, poster_path, overview, personal_comment)
-    VALUES ($1, $2, $3, $4, $5) RETURNING *;`
-    const values = [title, release_date, poster_path, overview, personal_comment]
-    client.query(sql, values).then((result) => {
-        console.log(result.rows);
-        res.status(201).json(result.rows)
-    }).catch()
-
-
-    // res.send("data recived");
+    for (const card of allCardsData) {
+        let sql =
+            "INSERT INTO card( card_name, card_category, card_level, job_title, img, portfolio) VALUES($1, $2, $3, $4, $5, $6) RETURNING *;"; // sql query
+        let values = [
+            card.card_name,
+            card.card_category,
+            card.card_level,
+            card.job_title,
+            card.img,
+            card.portfolio,
+        ];
+        client
+            .query(sql, values)
+            .then((result) => {
+                // console.log(result.rows);
+                return res.status(201).json(result.rows);
+            })
+            .catch((error) => {
+                handleServerError(error, req, res);
+                console.log(error.message);
+            });
+    }
 }
 
 function addNewCardHandler(req, res) {
@@ -73,10 +98,10 @@ function addNewCardHandler(req, res) {
     //const time= req.body.time;
     //const image= req.body.image;
 
-    const { title, release_date, poster_path, overview, personal_comment } = req.body //destructuring ES6
-    const sql = `INSERT INTO tMovie (title, release_date, poster_path, overview, personal_comment)
-    VALUES ($1, $2, $3, $4, $5) RETURNING *;`
-    const values = [title, release_date, poster_path, overview, personal_comment]
+    const { card_name, card_category, card_level, job_title, img, portfolio } = req.body //destructuring ES6
+    const sql = `INSERT INTO card (card_name, card_category, card_level, job_title, img, portfolio)
+    VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`
+    const values = [card_name, card_category, card_level, job_title, img, portfolio]
     client.query(sql, values).then((result) => {
         console.log(result.rows);
         res.status(201).json(result.rows)
@@ -107,7 +132,7 @@ function updateHandler(req, res) {
 }
 
 function getCardsHandler(req, res) {
-    const sql = `SELECT * FROM tMovie;`
+    const sql = `SELECT * FROM card;`
 
     client.query(sql).then((result) => {
         const data = result.rows
@@ -128,38 +153,38 @@ function getCardsByCategoryHandler(req, res) {
 
 function updateCardHandler(req, res) {
     // console.log(req.params)
-    let movieId = req.params.movieId;
-    let { title, release_date, poster_path, overview, personal_comment } = req.body;
-    let sql = `UPDATE tMovie
-         SET title = $1, release_date = $2, poster_path = $3, overview = $4, personal_comment = $5
-             WHERE id = $6;`;
-    let values = [title, release_date, poster_path, overview, personal_comment, movieId];
+    let cardId = req.params.id;
+    let { card_name, card_category, card_level, job_title, img, portfolio } = req.body;
+    let sql = `UPDATE card
+         SET card_name = $1, card_category = $2, card_level = $3, job_title = $4, img = $5, portfolio= $6
+             WHERE id = $7;`;
+    let values = [card_name, card_category, card_level, job_title, img, portfolio, cardId];
 
     client.query(sql, values)
         .then(result => {
-            console.log('tMovie updated:');
+            console.log('card updated:');
             res.status(200).send("successfully ubdate")
         })
         .catch(error => {
-            console.error('Error updating a tMovie:', error);
-            res.status(500).send('Error updating tMovie');
+            console.error('Error updating a card:', error);
+            res.status(500).send('Error updating card');
         });
 }
 
 function deleteCardHandler(req, res) {
     const { id } = req.params;
-    const sql = 'DELETE FROM tMovie WHERE id = $1;';
+    const sql = 'DELETE FROM card WHERE id = $1;';
     const valuse = [id];
 
     client.query(sql, valuse)
         .then(result => {
-            console.log('tMovie deleted:');
+            console.log('card deleted:');
             res.status(204).send("successfully Deleted");
 
         })
         .catch(error => {
-            console.error('Error deleting a movie:', error);
-            res.status(500).send('Error deleting movie');
+            console.error('Error deleting a card:', error);
+            res.status(500).send('Error deleting card');
         });
 }
 
