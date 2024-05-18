bc1ee32099bf738e3f8e332ff17c3caa16f6052e
Hassan-Ayman-SE <hsankraz66@gmail.com>
2024-05-18 09:09:40 +0000

    FINISH THESE ROUTES: /addLocallyData, /getCards/:category, /updateFavorite/:id, /deleteAllCards

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 const axios = require("axios").default;
 require("dotenv").config();
 
-const movieData = require("./Movie Data/data.json");
+const cardData = require("./Card_Data/data.json");
 
 //const apiKey = process.env.API_KEY;
 const pgUrl = process.env.DATABASE_URL;
-const PORT = process.env.PORT || 3002;//3000
+const PORT = process.env.PORT || 3002; //3000
 
 //for query in js file using pg package
 const { Client } = require("pg");
@@ -24,163 +24,236 @@ app.use(cors());
 //for parsing body
 app.use(express.json());
 
-app.get('/', homeHandler); //For Us , Locally
+app.get("/", homeHandler); //For Us , Locally
 
-app.post('/addLocallyData', addLocallyDataHandler); //For Us , Locally
-app.post('/addNewCard', addNewCardHandler);
+app.post("/addLocallyData", addLocallyDataHandler); //For Us , Locally *
+app.post("/addNewCard", addNewCardHandler);
 
-app.get('/getAllCards', getCardsHandler); //For Us , Locally
-app.get('/getCards/:category', getCardsByCategoryHandler);
+app.get("/getAllCards", getCardsHandler); //For Us , Locally
+app.get("/getCards/:category", getCardsByCategoryHandler); // *
 
-app.put('/updateCard/:id', updateCardHandler); //For Us , Locally
-app.put('/updateFavorite/:id', updateHandler);
+app.put("/updateCard/:id", updateCardHandler); //For Us , Locally
+app.put("/updateFavorite/:id", updateHandler); //*
 
-app.delete('/deleteCard/:id', deleteCardHandler); //For Us , Locallyrrr
-
+app.delete("/deleteCard/:id", deleteCardHandler); //For Us , Locallyrrr
+app.delete("/deleteAllCards", deleteAllCardsHandler); //For Us , Locally *
 app.use(handleServerError);
 app.use(handleNotFoundError);
 
-
-
-function homeHandler(req, res) {
-    res.send("WELCOME IN OUR ICONIC PROJECT");
+class Card {
+  constructor(card_name, card_category, card_level, job_title, img, portfolio) {
+    this.card_name = card_name;
+    this.card_category = card_category;
+    this.card_level = card_level;
+    this.job_title = job_title;
+    this.img = img;
+    this.portfolio = portfolio;
+  }
 }
 
+function homeHandler(req, res) {
+  res.send("WELCOME IN OUR ICONIC PROJECT");
+}
+/* 
+    Get data from data.json file 
+*/
 function addLocallyDataHandler(req, res) {
-    console.log(req.body)
-    //to collect the data:
-    //const title= req.body.title;
-    //const time= req.body.time;
-    //const image= req.body.image;
+  const allCardsData = cardData.map((item) => {
+    return new Card(
+      item.name,
+      item.category,
+      item.level,
+      item.job_title,
+      item.image,
+      item.portfolio
+    );
+  });
 
-    const { title, release_date, poster_path, overview, personal_comment } = req.body //destructuring ES6
-    const sql = `INSERT INTO tMovie (title, release_date, poster_path, overview, personal_comment)
-    VALUES ($1, $2, $3, $4, $5) RETURNING *;`
-    const values = [title, release_date, poster_path, overview, personal_comment]
-    client.query(sql, values).then((result) => {
-        console.log(result.rows);
-        res.status(201).json(result.rows)
-    }).catch()
+  /*
+  the response is sent only once, after all the insert operations have completed successfully. 
+  
+  */
+  const insertPromises = allCardsData.map((card) => {
+    let sql =
+      "INSERT INTO card (card_name, card_category, card_level, job_title, img, portfolio) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;";
+    let values = [
+      card.card_name,
+      card.card_category,
+      card.card_level,
+      card.job_title,
+      card.img,
+      card.portfolio,
+    ];
 
-
-    // res.send("data recived");
+    return client.query(sql, values);
+  });
+  /*
+The Promise.all function is called with the insertPromises array. 
+This function waits for all the promises to either fulfill or reject.
+*/
+  Promise.all(insertPromises)
+    .then((results) => {
+      // The inserted cards are collected in an array and sent as the response using res.status(201).json(insertedCards).
+      const insertedCards = results.map((result) => result.rows[0]);
+      res.status(201).json(insertedCards);
+    })
+    .catch((error) => {
+      errorHandler500(error, req, res);
+    });
 }
 
 function addNewCardHandler(req, res) {
-    console.log(req.body)
-    //to collect the data:
-    //const title= req.body.title;
-    //const time= req.body.time;
-    //const image= req.body.image;
+  console.log(req.body);
+  //to collect the data:
+  //const title= req.body.title;
+  //const time= req.body.time;
+  //const image= req.body.image;
 
-    const { title, release_date, poster_path, overview, personal_comment } = req.body //destructuring ES6
-    const sql = `INSERT INTO tMovie (title, release_date, poster_path, overview, personal_comment)
-    VALUES ($1, $2, $3, $4, $5) RETURNING *;`
-    const values = [title, release_date, poster_path, overview, personal_comment]
-    client.query(sql, values).then((result) => {
-        console.log(result.rows);
-        res.status(201).json(result.rows)
-    }).catch()
+  const { title, release_date, poster_path, overview, personal_comment } =
+    req.body; //destructuring ES6
+  const sql = `INSERT INTO tMovie (title, release_date, poster_path, overview, personal_comment)
+    VALUES ($1, $2, $3, $4, $5) RETURNING *;`;
+  const values = [title, release_date, poster_path, overview, personal_comment];
+  client
+    .query(sql, values)
+    .then((result) => {
+      console.log(result.rows);
+      res.status(201).json(result.rows);
+    })
+    .catch();
 
-
-    // res.send("data recived");
+  // res.send("data recived");
 }
 
+/*update the value of the isFav to true if the user clicked on add to favorites 
+    and to false if the user clicked on delete from favorites.
+*/
 function updateHandler(req, res) {
-    console.log(req.body)
-    //to collect the data:
-    //const title= req.body.title;
-    //const time= req.body.time;
-    //const image= req.body.image;
+  const id = req.params.id;
+  const { is_fav } = req.body;
 
-    const { title, release_date, poster_path, overview, personal_comment } = req.body //destructuring ES6
-    const sql = `INSERT INTO tMovie (title, release_date, poster_path, overview, personal_comment)
-    VALUES ($1, $2, $3, $4, $5) RETURNING *;`
-    const values = [title, release_date, poster_path, overview, personal_comment]
-    client.query(sql, values).then((result) => {
-        console.log(result.rows);
-        res.status(201).json(result.rows)
-    }).catch()
+  const sql = `UPDATE card
+    SET is_fav=$1
+    WHERE id=$2 RETURNING *;`;
 
-
-    // res.send("data recived");
+  let values = [is_fav, id];
+  client
+    .query(sql, values)
+    .then((result) => {
+      res.status(200).json(result.rows);
+    })
+    .catch((error) => {
+      handleServerError(error, req, res);
+      console.log(error);
+    });
 }
 
 function getCardsHandler(req, res) {
-    const sql = `SELECT * FROM tMovie;`
+  const sql = `SELECT * FROM card;`;
 
-    client.query(sql).then((result) => {
-        const data = result.rows
-        res.json(data)
+  client
+    .query(sql)
+    .then((result) => {
+      const data = result.rows;
+      res.json(data);
     })
-        .catch()
+    .catch();
 }
 
 function getCardsByCategoryHandler(req, res) {
-    const sql = `SELECT * FROM tMovie;`
-
-    client.query(sql).then((result) => {
-        const data = result.rows
-        res.json(data)
+  const category = req.params.category;
+  const sql = `SELECT * FROM card WHERE card_category = $1;`;
+  let values = [category];
+  client
+    .query(sql, values)
+    .then((result) => {
+      const data = result.rows;
+      res.json(data);
     })
-        .catch()
+    .catch();
 }
 
 function updateCardHandler(req, res) {
-    // console.log(req.params)
-    let movieId = req.params.movieId;
-    let { title, release_date, poster_path, overview, personal_comment } = req.body;
-    let sql = `UPDATE tMovie
+  // console.log(req.params)
+  let movieId = req.params.movieId;
+  let { title, release_date, poster_path, overview, personal_comment } =
+    req.body;
+  let sql = `UPDATE tMovie
          SET title = $1, release_date = $2, poster_path = $3, overview = $4, personal_comment = $5
              WHERE id = $6;`;
-    let values = [title, release_date, poster_path, overview, personal_comment, movieId];
+  let values = [
+    title,
+    release_date,
+    poster_path,
+    overview,
+    personal_comment,
+    movieId,
+  ];
 
-    client.query(sql, values)
-        .then(result => {
-            console.log('tMovie updated:');
-            res.status(200).send("successfully ubdate")
-        })
-        .catch(error => {
-            console.error('Error updating a tMovie:', error);
-            res.status(500).send('Error updating tMovie');
-        });
+  client
+    .query(sql, values)
+    .then((result) => {
+      console.log("tMovie updated:");
+      res.status(200).send("successfully ubdate");
+    })
+    .catch((error) => {
+      console.error("Error updating a tMovie:", error);
+      res.status(500).send("Error updating tMovie");
+    });
 }
 
 function deleteCardHandler(req, res) {
-    const { id } = req.params;
-    const sql = 'DELETE FROM tMovie WHERE id = $1;';
-    const valuse = [id];
+  const { id } = req.params;
+  const sql = "DELETE FROM tMovie WHERE id = $1;";
+  const valuse = [id];
 
-    client.query(sql, valuse)
-        .then(result => {
-            console.log('tMovie deleted:');
-            res.status(204).send("successfully Deleted");
-
-        })
-        .catch(error => {
-            console.error('Error deleting a movie:', error);
-            res.status(500).send('Error deleting movie');
-        });
-}
-
-function handleServerError(req, res) {
-    res.status(500).json({
-        status: 500,
-        responseText: 'Sorry, something went wrong'
+  client
+    .query(sql, valuse)
+    .then((result) => {
+      console.log("tMovie deleted:");
+      res.status(204).send("successfully Deleted");
+    })
+    .catch((error) => {
+      console.error("Error deleting a movie:", error);
+      res.status(500).send("Error deleting movie");
     });
 }
 
-function handleNotFoundError(req, res) {
-    res.status(404).json({
-        status: 404,
-        responseText: 'Page not found'
+function deleteAllCardsHandler(req, res) {
+  const sql = "DELETE FROM card";
+
+  client
+    .query(sql)
+    .then(() => {
+      res.status(200).json({ message: "All data deleted successfully" });
+    })
+    .catch((error) => {
+      handleServerError(error, req, res);
+      console.log(error);
     });
+}
+function handleServerError(error, req, res) {
+  const err = {
+    status: 500,
+    massage: error,
+  };
+  res.status(500).send(err);
+}
+
+function handleNotFoundError(error, req, res) {
+  const err = {
+    status: 404,
+    massage: error,
+  };
+  res.status(404).send(err);
 }
 
 //listener
-client.connect().then(() => {
+client
+  .connect()
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is listening ${PORT}`);
-    })
-}
-).catch()
+      console.log(`Server is listening ${PORT}`);
+    });
+  })
+  .catch();
